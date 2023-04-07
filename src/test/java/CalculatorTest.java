2cecad6688431db2ae3de171df9101d5046c044f
Vladislav_Kanashevsky <kanash1998@gmail.com>
2023-04-07 23:02:44 +0300

    day 14 homework correct task CalculatorTest

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -34,7 +34,6 @@ public class CalculatorTest {
         Assert.assertEquals("Addition returned wrong result: ", 0, calculator.multiply(0, 0));
         Assert.assertEquals("Addition returned wrong result: ", 0, calculator.multiply(2147483647, 0));
         Assert.assertEquals("Addition returned wrong result: ", -2147483648, calculator.multiply(-1, -2147483648));
-        Assert.assertEquals("Addition returned wrong result: ", -2147483647, calculator.multiply(-1, -2147483647));
     }
 
     @Test
@@ -43,5 +42,6 @@ public class CalculatorTest {
         Assert.assertEquals("Addition returned wrong result: ", 1, calculator.divide(2147483646, 2147483646));
         Assert.assertEquals("Addition returned wrong result: ", 0, calculator.divide(0, 2147483647));
         Assert.assertEquals("Addition returned wrong result: ", 1, calculator.divide(-2147483648, -2147483648));
+
     }
 }
