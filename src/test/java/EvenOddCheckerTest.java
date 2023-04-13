3a0d6a1e0fc335cffca8b840056127620849039c
Vladislav_Kanashevsky <kanash1998@gmail.com>
2023-04-13 16:58:27 +0300

    day 14 homework correct

--- src/test/java/EvenOddCheckerTest.java
+++ src/test/java/EvenOddCheckerTest.java
@@ -6,17 +6,33 @@ public class EvenOddCheckerTest {
     EvenOddChecker evenOddChecker = new EvenOddChecker();
 
     @Test
-    public void EvenOddCheckerTestOne() {
+    public void EvenOddCheckerOneTest() {
         Assert.assertTrue(evenOddChecker.check(2147483646));
+    }
+
+    @Test
+    public void EvenOddCheckerTwoTest() {
         Assert.assertTrue(evenOddChecker.check(-2147483648));
+    }
+
+    @Test
+    public void EvenOddCheckerThreeTest() {
         Assert.assertTrue(evenOddChecker.check(0));
     }
 
     @Test
-    public void EvenOddCheckerTestTwo() {
+    public void EvenOddCheckerFourTest() {
         Assert.assertFalse(evenOddChecker.check(2147483647));
-        Assert.assertFalse(evenOddChecker.check(-2147483647));
-        Assert.assertFalse(evenOddChecker.check(-1));
+    }
 
+    @Test
+    public void EvenOddCheckerFiveTest() {
+        Assert.assertFalse(evenOddChecker.check(-2147483647));
+    }
+
+    @Test
+    public void EvenOddCheckerSixTest() {
+        Assert.assertFalse(evenOddChecker.check(-1));
+        
     }
 }
