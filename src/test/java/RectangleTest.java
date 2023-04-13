64c2ccaba59ab1fb020f7f4e9da0fe02d09c06c0
Vladislav_Kanashevsky <kanash1998@gmail.com>
2023-04-13 17:01:54 +0300

    day 14 homework correct

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -26,8 +26,16 @@ public class RectangleTest {
     public void RectangleSetWidthTest() {
         rectangleMax.setWidth(1.7E+308);
         Assert.assertEquals(1.7E+308, rectangleMax.getWidth(), 0);
+    }
+
+    @Test
+    public void RectangleSetWidthTwoTest() {
         rectangleMax.setWidth(0);
         Assert.assertEquals(0, rectangleMax.getWidth(), 0);
+    }
+
+    @Test
+    public void RectangleSetWidthThreeTest() {
         rectangleMax.setWidth(1.7E-308);
         Assert.assertEquals(1.7E-308, rectangleMax.getWidth(), 0);
     }
@@ -36,10 +44,17 @@ public class RectangleTest {
     public void RectangleSetLengthTest() {
         rectangleMax.setLength(1.7E+308);
         Assert.assertEquals(1.7E+308, rectangleMax.getLength(), 0);
+    }
+
+    @Test
+    public void RectangleSetLengthTwoTest() {
         rectangleMax.setLength(0);
         Assert.assertEquals(0, rectangleMax.getLength(), 0);
+    }
+
+    @Test
+    public void RectangleSetLengthThreeTest() {
         rectangleMax.setLength(1.7E-308);
         Assert.assertEquals(1.7E-308, rectangleMax.getLength(), 0);
-
     }
 }
