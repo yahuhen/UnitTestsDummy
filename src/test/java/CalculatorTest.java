f1e57e87edc61fdafac5eb3758485b13268a5e5c
Vladislav_Kanashevsky <kanash1998@gmail.com>
2023-04-13 16:51:49 +0300

    day 14 homework correct task

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,47 +1,128 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class CalculatorTest {
 
     Calculator calculator = new Calculator();
 
+
     @Test
     public void CalculatorAddTest() {
         Assert.assertEquals("Addition returned wrong result: ", 2147483647, calculator.add(1, 2147483646));
+    }
+
+    @Test
+    public void CalculatorAddTwoTest() {
         Assert.assertEquals("Addition returned wrong result: ", 2147483646, calculator.add(1, 2147483645));
+    }
+
+    @Test
+    public void CalculatorAddThreeTest() {
         Assert.assertEquals("Addition returned wrong result: ", 0, calculator.add(0, 0));
+    }
+
+    @Test
+    public void CalculatorAddFourTest() {
         Assert.assertEquals("Addition returned wrong result: ", 0, calculator.add(-2147483647, 2147483647));
+    }
+
+    @Test
+    public void CalculatorAddFiveTest() {
         Assert.assertEquals("Addition returned wrong result: ", -2147483648, calculator.add(-1, -2147483647));
+    }
+
+    @Test
+    public void CalculatorAddSixTest() {
         Assert.assertEquals("Addition returned wrong result: ", -2147483647, calculator.add(-1, -2147483646));
     }
 
     @Test
     public void CalculatorSubtractTest() {
         Assert.assertEquals("Addition returned wrong result: ", 1, calculator.subtract(2147483647, 2147483646));
+    }
+
+    @Test
+    public void CalculatorSubtractTwoTest() {
         Assert.assertEquals("Addition returned wrong result: ", 1, calculator.subtract(2147483646, 2147483645));
+    }
+
+    @Test
+    public void CalculatorSubtractThreeTest() {
         Assert.assertEquals("Addition returned wrong result: ", 0, calculator.subtract(0, 0));
+    }
+
+    @Test
+    public void CalculatorSubtractFourTest() {
         Assert.assertEquals("Addition returned wrong result: ", 0, calculator.subtract(2147483647, 2147483647));
+    }
+
+    @Test
+    public void CalculatorSubtractFiveTest() {
         Assert.assertEquals("Addition returned wrong result: ", 0, calculator.subtract(-2147483647, -2147483647));
+    }
+
+    @Test
+    public void CalculatorSubtractSixTest() {
         Assert.assertEquals("Addition returned wrong result: ", -1, calculator.subtract(2147483646, 2147483647));
+    }
+
+    @Test
+    public void CalculatorSubtractSevenTest() {
         Assert.assertEquals("Addition returned wrong result: ", -1, calculator.subtract(2147483645, 2147483646));
+    }
+
+    @Test
+    public void CalculatorSubtractEightTest() {
         Assert.assertEquals("Addition returned wrong result: ", -1, calculator.subtract(2147483647, -2147483648));
     }
 
     @Test
     public void CalculatorMultiplyTest() {
         Assert.assertEquals("Addition returned wrong result: ", 2147483647, calculator.multiply(1, 2147483647));
+    }
+
+    @Test
+    public void CalculatorMultiplyTwoTest() {
         Assert.assertEquals("Addition returned wrong result: ", 2147483646, calculator.multiply(1, 2147483646));
+    }
+
+    @Test
+    public void CalculatorMultiplyThreeTest() {
         Assert.assertEquals("Addition returned wrong result: ", 0, calculator.multiply(0, 0));
+    }
+
+    @Test
+    public void CalculatorMultiplyFourTest() {
         Assert.assertEquals("Addition returned wrong result: ", 0, calculator.multiply(2147483647, 0));
+    }
+
+    @Test
+    public void CalculatorMultiplyFiveTest() {
         Assert.assertEquals("Addition returned wrong result: ", -2147483648, calculator.multiply(-1, -2147483648));
     }
 
     @Test
     public void CalculatorDivideTest() {
         Assert.assertEquals("Addition returned wrong result: ", 1, calculator.divide(2147483647, 2147483647));
-        Assert.assertEquals("Addition returned wrong result: ", 1, calculator.divide(2147483646, 2147483646));
-        Assert.assertEquals("Addition returned wrong result: ", 0, calculator.divide(0, 2147483647));
-        Assert.assertEquals("Addition returned wrong result: ", 1, calculator.divide(-2147483648, -2147483648));
+    }
 
+    @Test
+    public void CalculatorDivideTwoTest() {
+        Assert.assertEquals("Addition returned wrong result: ", 1, calculator.divide(2147483646, 2147483646));
+    }
+
+    @Test
+    public void CalculatorDivideThreeTest() {
+        Assert.assertEquals("Addition returned wrong result: ", 0, calculator.divide(0, 2147483647));
+    }
+
+    @Test
+    public void CalculatorDivideFourTest() {
+        Assert.assertEquals("Addition returned wrong result: ", 1, calculator.divide(-2147483648, -2147483648));
     }
 }
