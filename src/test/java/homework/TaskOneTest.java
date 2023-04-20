d0ccb90fead7abbbc577fe29c0ddc31c494a0be1
kolator <katovich.mikalai@gmail.com>
2023-04-20 11:59:19 +0200

    microUpdate

--- src/test/java/homework/TaskOneTest.java
+++ src/test/java/homework/TaskOneTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -21,33 +22,25 @@ public class TaskOneTest {
         driver.get("https://www.booking.com/");
         driver.findElement(By.xpath("//*[@id='onetrust-accept-btn-handler']")).click();
 
-        // Ввод данных для поиска отелей в Париже
+        // отелеи в Париже
         WebElement cityField = driver.findElement(By.name("ss"));
         cityField.sendKeys("Париж");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-
-        // Открываем календарь
+        // календарь
         WebElement dates = driver.findElement(By.xpath("//*[@id=\"indexsearch\"]//button[@data-testid=\"date-display-field-start\"]"));
         dates.click();
 
         //Выбираем даты
         LocalDate checkinDate = LocalDate.now().plusDays(3);
         LocalDate checkoutDate = checkinDate.plusDays(7);
-
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        WebElement checkinDateField = driver.findElement(By.xpath(String.format("//span[@data-date='%s']", checkinDate.format(dateFormat))));
-        checkinDateField.click();
-
-        WebElement checkoutDateField = driver.findElement(By.xpath(String.format("//span[@data-date='%s']", checkoutDate.format(dateFormat))));
-        checkoutDateField.click();
-
+        driver.findElement(By.xpath(String.format("//span[@data-date='%s']", checkinDate.format(dateFormat)))).click();
+        driver.findElement(By.xpath(String.format("//span[@data-date='%s']", checkoutDate.format(dateFormat)))).click();
 
         //номера + люди
-        WebElement optionsButton = driver.findElement(By.xpath("//*[@data-testid='occupancy-config']"));
-        optionsButton.click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.findElement(By.xpath("//*[@data-testid='occupancy-config']")).click();
 
         WebElement clickElement = driver.findElement(By.xpath("//*[@id='group_adults']/following-sibling::div/button[2]"));
         clickElement.click();
@@ -56,5 +49,8 @@ public class TaskOneTest {
 
         //сабмит
         driver.findElement(By.xpath("//*[@type='submit']")).click();
+
+
+        
     }
 }
